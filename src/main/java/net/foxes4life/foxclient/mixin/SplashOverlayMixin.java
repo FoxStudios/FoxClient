b7a5e80542cbd6e3d985da9fedbf40f1a235f17b
RoootTheFox <rooot@juligames.net>
2022-02-24 21:26:36 +0100

    splash colors

--- src/main/java/net/foxes4life/foxclient/mixin/SplashOverlayMixin.java
+++ src/main/java/net/foxes4life/foxclient/mixin/SplashOverlayMixin.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Overlay;
 import net.minecraft.client.gui.screen.SplashOverlay;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
@@ -18,34 +19,24 @@ public abstract class SplashOverlayMixin extends Overlay {
     protected SplashOverlayMixin() {
     }
 
+    @Final @Mutable @Shadow
+    private static int MOJANG_RED;
+
+    @Final @Mutable @Shadow
+    private static int MONOCHROME_BLACK;
+
     private static final Identifier BACKGROUND = new Identifier("foxclient", "textures/ui/title/bg.png");
 
     @Mutable
     @Final
     @Shadow static Identifier LOGO = new Identifier("textures/gui/title/mojangstudios.png");
 
-    @Shadow @Final private MinecraftClient client;
-
     @Inject(at = @At("TAIL"), method = "init")
     private static void init(MinecraftClient client, CallbackInfo ci) {
         LOGO = new Identifier("foxclient", "textures/foxclientsplash.png");
         client.getTextureManager().registerTexture(BACKGROUND, TextureUtil.fromIdentifier(BACKGROUND));
-    }
 
-    /*
-    @Inject(at = @At("HEAD"), method = "render")
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        int height = this.client.getWindow().getScaledHeight();
-        int width = this.client.getWindow().getScaledWidth();
-        //ci.cancel();
-
-        MinecraftClient.getInstance().getTextureManager().bindTexture(BACKGROUND);
-        RenderSystem.setShader(GameRenderer::getPositionTexShader);
-        RenderSystem.setShaderTexture(1, BACKGROUND);
-        RenderSystem.enableBlend();
-        RenderSystem.blendFunc(GlStateManager.SrcFactor.SRC_ALPHA, GlStateManager.DstFactor.ONE_MINUS_SRC_ALPHA);
-        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-        drawTexture(matrices, 0, 0, width, height, 0.0F, 0.0F, 16, 128, 16, 128);
+        MOJANG_RED = ColorHelper.Argb.getArgb(255, 32, 32, 32);
+        MONOCHROME_BLACK = ColorHelper.Argb.getArgb(255, 16, 16, 16);
     }
-    */
 }
