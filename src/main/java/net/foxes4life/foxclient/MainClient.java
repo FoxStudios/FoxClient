a6c33c3929c4b9cb90e9e6be08debaf923dddbd7
Flux <flustixshinu@gmail.com>
2022-03-07 22:04:25 +0100

    freelook

--- src/main/java/net/foxes4life/foxclient/MainClient.java
+++ src/main/java/net/foxes4life/foxclient/MainClient.java
@@ -8,6 +8,8 @@ import net.foxes4life.foxclient.rpc.DiscordInstance;
 import net.foxes4life.foxclient.rpc.DiscordMinecraftClient;
 import net.foxes4life.foxclient.rpc.PresenceUpdater;
 import net.foxes4life.foxclient.util.ZoomUtils;
+import net.foxes4life.foxclient.util.freelook.Freelook;
+import net.foxes4life.foxclient.util.freelook.FreelookUtils;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
@@ -18,6 +20,7 @@ public class MainClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ZoomUtils.initZoom();
+        FreelookUtils.init();
         KeyBindingHelper.registerKeyBinding(toggleHud);
 
         ClientLifecycleEvents.CLIENT_STARTED.register(client -> {
@@ -36,6 +39,10 @@ public class MainClient implements ClientModInitializer {
                             Main.config_instance.getEntry("misc", "hud-enabled").setValue(true));
                 }
             }
+        });
+
+        ClientTickEvents.START_CLIENT_TICK.register((client) -> {
+            FreelookUtils.tick();
         });
     }
 }
---
+++ src/main/java/net/foxes4life/foxclient/util/freelook/Freelook.java
@@ -0,0 +1,8 @@
+package net.foxes4life.foxclient.util.freelook;
+
+public interface Freelook {
+    float getCameraX();
+    float getCameraY();
+    void setCameraX(float x);
+    void setCameraY(float y);
+}
