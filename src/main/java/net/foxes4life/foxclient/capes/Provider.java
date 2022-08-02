a6cdbbc2ce5e571b09ca14e4dc0963e03d446622
RoootTheFox <rooot@foxes4life.net>
2022-08-02 22:09:58 +0200

    fix lerping on high fps

--- src/main/java/net/foxes4life/foxclient/capes/Provider.java
+++ src/main/java/net/foxes4life/foxclient/capes/Provider.java
@@ -14,6 +14,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.mojang.authlib.GameProfile;
 
+import net.foxes4life.foxclient.Main;
 import net.foxes4life.foxclient.util.Http;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.texture.NativeImage;
@@ -32,8 +33,8 @@ public final class Provider {
             Identifier existingCape = capes.get(player.getName());
             if(existingCape != null) {
                 System.out.println("hi");
-                callback.onTexAvail(existingCape);
-                return;
+                //callback.onTexAvail(existingCape);
+                //return;
             }
 
             if(!Provider.tryUrl(player, callback, "https://client.foxes4life.net/capes/get.php?uuid=" + player.getId().toString().replace("-", ""))) {
@@ -54,7 +55,7 @@ public final class Provider {
     // If this fails, it'll return false, and let us try another url.
     private static boolean tryUrl(GameProfile player, CapeTextureAvailableCallback callback, String urlFrom) {
         try {
-            System.out.println("[FoxClient/Cape/Provider] trying url: " + urlFrom);
+            //Main.LOGGER.debug("[FoxClient/Cape/Provider] trying url: " + urlFrom);
             HttpResponse response = Http.get(urlFrom);
             if(response != null && response.getStatusLine().getStatusCode() == 200) {
                 String responseBody = Http.getResponseBody(response);
@@ -70,16 +71,15 @@ public final class Provider {
                             .registerDynamicTexture("foxclient_" + player.getId().toString().replace("-", ""),
                             new NativeImageBackedTexture(cape));
                     capes.put(player.getName(), id);
-                    System.out.println("put " + id.toString() + " for player " + player.getName());
+                    Main.LOGGER.debug("put " + id.toString() + " for player " + player.getName());
                     callback.onTexAvail(id);
                 }
             } else {
                 if(response != null && response.getStatusLine().getStatusCode() == 404) {
-                    System.out.println("[FoxClient/Cape/Provider] no cape");
                     // player has no cape
                     return true;
                 }
-                System.out.println("[FoxClient/Cape/Provider] request failed: " + player.getId().toString());
+                Main.LOGGER.debug("[FoxClient/Cape/Provider] request failed: " + player.getId().toString());
             }
         } catch (IOException e) {
             e.printStackTrace();
