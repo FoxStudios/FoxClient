3c15e37c0d6ad9010e65f8b553694bfba2a87cf1
Flux <flustixshinu@gmail.com>
2022-09-09 21:38:26 +0200

    draw a tail to *every* player in the tablist

---
+++ src/main/java/net/foxes4life/foxclient/mixin/PlayerListHudMixin.java
@@ -0,0 +1,34 @@
+package net.foxes4life.foxclient.mixin;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.foxes4life.foxclient.Main;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.PlayerListHud;
+import net.minecraft.client.network.PlayerListEntry;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.scoreboard.Scoreboard;
+import net.minecraft.scoreboard.ScoreboardObjective;
+import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerListHud.class)
+public class PlayerListHudMixin extends DrawableHelper {
+    final Identifier FOXCLIENT_TAIL = new Identifier("foxclient", "textures/ui/branding/tail.png");
+
+
+    @Inject(at = @At("TAIL"), method = "renderLatencyIcon")
+    private void renderLatencyIcon(MatrixStack matrices, int width, int x, int y, PlayerListEntry entry, CallbackInfo ci) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, 1.0f);
+        RenderSystem.setShaderTexture(0, FOXCLIENT_TAIL);
+        this.setZOffset(this.getZOffset() + 101);
+        drawTexture(matrices, x + width - 20, y, 0, 8, 8, 8, 8, 8);
+        this.setZOffset(this.getZOffset() - 101);
+
+    }
+}
