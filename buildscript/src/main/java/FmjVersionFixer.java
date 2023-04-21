e03559f225f3419dcfc408803f44b7b66a070040
rooot <hey@rooot.gay>
2023-04-21 15:25:44 +0200

    fix: ok passi
    Signed-off-by: rooot <hey@rooot.gay>

--- buildscript/src/main/java/FmjVersionFixer.java
+++ buildscript/src/main/java/FmjVersionFixer.java
@@ -18,7 +18,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 
-
 public class FmjVersionFixer implements Processor {
 	private final Buildscript parent;
 
