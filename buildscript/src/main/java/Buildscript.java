52080ce86a0240490725ea1b151fdc7fda0ac44f
rooot <hey@rooot.gay>
2023-04-21 17:28:59 +0200

    fix: dependency bundling and mod/jar versioning
    Signed-off-by: rooot <hey@rooot.gay>

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -1,3 +1,4 @@
+import io.github.coolcrabs.brachyura.dependency.JavaJarDependency;
 import io.github.coolcrabs.brachyura.fabric.FabricLoader;
 import io.github.coolcrabs.brachyura.fabric.FabricMaven;
 import io.github.coolcrabs.brachyura.fabric.SimpleFabricProject;
@@ -8,11 +9,14 @@ import io.github.coolcrabs.brachyura.maven.Maven;
 import io.github.coolcrabs.brachyura.maven.MavenId;
 import io.github.coolcrabs.brachyura.minecraft.Minecraft;
 import io.github.coolcrabs.brachyura.minecraft.VersionMeta;
+import io.github.coolcrabs.brachyura.processing.ProcessorChain;
 import net.fabricmc.mappingio.tree.MappingTree;
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Constants;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 
 public class Buildscript extends SimpleFabricProject {
 
@@ -24,6 +28,12 @@ public class Buildscript extends SimpleFabricProject {
 	static final String MODMENU_VERSION    = "6.2.0";
 	static final String KONFIG_VERSION     = "1.5.0";
 	static final String JAVA_WS_VERSION    = "1.5.3";
+
+	private final ArrayList<Path> dependencyJars = new ArrayList<>();
+
+	public ArrayList<Path> getDependencyJars() {
+		return dependencyJars;
+	}
 
 	@Override
 	public VersionMeta createMcVersion() {
@@ -45,27 +55,48 @@ public class Buildscript extends SimpleFabricProject {
 		return new FabricLoader(FabricMaven.URL, FabricMaven.loader(FABRIC_LOADER));
 	}
 
-	public String commitHash() {
-		String commitHash;
+	@Override
+	public String getVersion() {
+		// ${version}-rev.#branch#.#githash#
+		return FOXCLIENT_VERSION + "-" + MC_VERSION + "-rev." + getGitVersionInformation();
+	}
+
+	public String getGitVersionInformation() {
 		Git git;
+
+		String branch;
+		String hash;
+		boolean dirty;
+
 		try {
 			git = Git.open(getProjectDir().toFile());
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 		try {
-			commitHash = git.getRepository().parseCommit(git.getRepository().resolve(Constants.HEAD).toObjectId()).getName().substring(0, 8);
-		} catch (IOException e) {
+			hash = git.getRepository().parseCommit(git.getRepository().resolve(Constants.HEAD).toObjectId()).getName().substring(0, 8);
+			branch = git.getRepository().getBranch();
+			dirty = !git.status().call().isClean();
+		} catch (IOException | GitAPIException e) {
 			throw new RuntimeException(e);
 		}
 		git.close();
 
-		return commitHash;
+		return branch + "." + hash + (dirty ? "-dirty" : "");
 	}
 
 	@Override
 	public Path getBuildJarPath() {
-		return getBuildLibsDir().resolve(getModId() + "-" + FOXCLIENT_VERSION  + "-mc-" + MC_VERSION + "#" + commitHash() + ".jar");
+		return getBuildLibsDir().resolve(getModId() + "-" + getVersion() + ".jar");
+	}
+
+	private void bundle(JavaJarDependency d) {
+		dependencyJars.add(d.jar);
+	}
+
+	@Override
+	public ProcessorChain resourcesProcessingChain() {
+		return new ProcessorChain(super.resourcesProcessingChain(), new ResourceProcessor(this));
 	}
 
 	@Override
@@ -85,11 +116,11 @@ public class Buildscript extends SimpleFabricProject {
 		d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api:fabric-api:" + FABRIC_API_VERSION), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
 
 		// the version for discord-rpc is hardcoded because it's not getting updated anymore anyway
-		d.addMaven("https://jitpack.io", new MavenId("com.github.Vatuu:discord-rpc:1.6.2"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
-		d.addMaven(Maven.MAVEN_CENTRAL, new MavenId("org.java-websocket:Java-WebSocket:" + JAVA_WS_VERSION), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
-		d.addMaven("https://maven.foxes4life.net", new MavenId("net.foxes4life:konfig:" + KONFIG_VERSION), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		bundle(d.addMaven("https://jitpack.io", new MavenId("com.github.Vatuu:discord-rpc:1.6.2"), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE));
+		bundle(d.addMaven(Maven.MAVEN_CENTRAL, new MavenId("org.java-websocket:Java-WebSocket:" + JAVA_WS_VERSION), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE));
+		bundle(d.addMaven("https://maven.foxes4life.net", new MavenId("net.foxes4life:konfig:" + KONFIG_VERSION), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE));
 
-		jij(d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api:fabric-api:" + FABRIC_API_VERSION), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME));
-		jij(d.addMaven("https://maven.terraformersmc.com/releases/", new MavenId("com.terraformersmc:modmenu:" + MODMENU_VERSION), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME));
+		d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api:fabric-api:" + FABRIC_API_VERSION), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME);
+		d.addMaven("https://maven.terraformersmc.com/releases/", new MavenId("com.terraformersmc:modmenu:" + MODMENU_VERSION), ModDependencyFlag.COMPILE, ModDependencyFlag.RUNTIME);
 	}
 }
--- buildscript/src/main/java/ResourceProcessor.java
+++ buildscript/src/main/java/ResourceProcessor.java
@@ -2,12 +2,14 @@
  * This file was "inspired" (stolen) from https://github.com/IrisShaders/Iris
  * It is licensed under the LGPL-3.0 license.
  * https://github.com/IrisShaders/Iris/blob/1.19.4/LICENSE
+ * Modified to add dependencies into the jar thingy
 */
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import io.github.coolcrabs.brachyura.processing.ProcessingEntry;
+import io.github.coolcrabs.brachyura.processing.ProcessingId;
 import io.github.coolcrabs.brachyura.processing.ProcessingSink;
 import io.github.coolcrabs.brachyura.processing.Processor;
 import io.github.coolcrabs.brachyura.util.GsonUtil;
@@ -16,14 +18,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.zip.ZipFile;
 
-public class FmjVersionFixer implements Processor {
+public class ResourceProcessor implements Processor {
 	private final Buildscript parent;
 
-	public FmjVersionFixer(Buildscript parent) {
+	public ResourceProcessor(Buildscript parent) {
 		this.parent = parent;
 	}
+
+	private final String[] ignoredDependencyPaths = new String[] {
+			"com/sun/jna", /* fixes foxclient causing issues with simplevoicehat and possibly more due to discord-rpc bundling jna */
+	};
 
 	@Override
 	public void process(Collection<ProcessingEntry> inputs, ProcessingSink sink) throws IOException {
@@ -36,7 +45,24 @@ public class FmjVersionFixer implements Processor {
 				}
 
 				fabricModJson.addProperty("version", parent.getVersion());
-				//fabricModJson.get("mixins").getAsJsonArray().remove(new JsonPrimitive("mixins.iris.devenvironment.json"));
+
+				Collection<Path> jij = parent.getDependencyJars();
+				for (Path jar : jij) {
+					ZipFile zip = new ZipFile(jar.toFile());
+					zip.stream()
+							.filter(a -> Arrays.stream(ignoredDependencyPaths).parallel().noneMatch(a.getName()::contains))
+							.forEach(entry -> {
+						if (!entry.getName().startsWith("META-INF") && !entry.isDirectory()) {
+							sink.sink(() -> {
+								try {
+									return zip.getInputStream(entry);
+								} catch (IOException ex) {
+									throw new RuntimeException(ex);
+								}
+							}, new ProcessingId(entry.getName(), e.id.source));
+						}
+					});
+				}
 
 				sink.sink(() -> GsonUtil.toIs(fabricModJson, gson), e.id);
 			} else {
