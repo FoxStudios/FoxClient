e9353cb0ecb6e1225b84f2eeeb3f89ebaa0e0dbe
Flux <flustixshinu@gmail.com>
2023-06-28 15:00:19 +0200

    Simple block display

---
+++ src/main/java/net/foxes4life/foxclient/gui/BlockHud.java
@@ -0,0 +1,102 @@
+package net.foxes4life.foxclient.gui;
+
+import net.foxes4life.foxclient.util.TextUtils;
+import net.foxes4life.foxclient.util.draw.Anchor;
+import net.foxes4life.foxclient.util.draw.AnchoredBounds;
+import net.foxes4life.foxclient.util.draw.DrawUtils;
+import net.minecraft.block.AirBlock;
+import net.minecraft.block.Block;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerInteractionManager;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.render.model.json.ModelTransformationMode;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.MutableText;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.RaycastContext;
+
+import java.awt.*;
+
+public class BlockHud {
+    private final MinecraftClient client;
+    private Block lastBlock;
+
+    private float alpha = 0;
+    private float width = 0;
+
+    public BlockHud(MinecraftClient client) {
+        this.client = client;
+    }
+
+    public void render(DrawContext context) {
+        Block block = getBlock();
+        boolean show = false;
+
+        if (block != null && !(block instanceof AirBlock)) {
+            lastBlock = block;
+            show = true;
+        }
+        else {
+            block = lastBlock;
+        }
+
+        // if the block is still null, we don't want to render anything
+        if (block == null) return;
+
+        float delta = client.getTickDelta();
+        float alphaDelta = show ? .4f : -.4f;
+        alpha = Math.max(0, Math.min(1, alpha + alphaDelta * delta));
+
+        // don't even bother
+        if (alpha == 0) return;
+
+        String translationKey = block.getTranslationKey();
+        MutableText text = TextUtils.translatable(translationKey);
+        int w = client.textRenderer.getWidth(text);
+
+        // transition the width
+        width = MathHelper.lerp(.8f * delta, width, w);
+
+        AnchoredBounds bounds = new AnchoredBounds(0, 0, (int)width, 14, client.getWindow().getScaledWidth(), client.getWindow().getScaledHeight(), Anchor.TopCenter, Anchor.TopCenter);
+
+        Color backgroundColor = new Color(0, 0, 0, .5f * alpha);
+        int backgroundColorInt = backgroundColor.getRGB();
+
+        DrawUtils.drawRect(context, bounds, backgroundColorInt);
+        DrawUtils.drawRect(context, bounds.x - 5, bounds.y, 5, bounds.height, backgroundColorInt);
+        DrawUtils.drawRect(context, bounds.x + bounds.width, bounds.y, 5, bounds.height, backgroundColorInt);
+        DrawUtils.drawRect(context, bounds.x, bounds.y + bounds.height, bounds.width, 5, backgroundColorInt);
+
+        Color textColor = new Color(1, 1, 1, alpha);
+        context.drawText(client.textRenderer, text, bounds.x, 3, textColor.getRGB(), false);
+    }
+
+    private Block getBlock() {
+        ClientWorld world = client.world;
+        if (world == null) return null;
+
+        Entity camera = client.getCameraEntity();
+        if (camera == null) return null;
+
+        ClientPlayerInteractionManager interactionManager = client.interactionManager;
+        if (interactionManager == null) return null;
+
+        float maxRange = interactionManager.getReachDistance();
+
+        Vec3d viewVector = camera.getRotationVec(client.getTickDelta());
+        Vec3d startVector = camera.getCameraPosVec(client.getTickDelta());
+        Vec3d endVector = startVector.add(viewVector.x * maxRange, viewVector.y * maxRange, viewVector.z * maxRange);
+
+        RaycastContext raycastContext = new RaycastContext(startVector, endVector, RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.SOURCE_ONLY, camera);
+        BlockPos blockPos = world.raycast(raycastContext).getBlockPos();
+
+        return world.getBlockState(blockPos).getBlock();
+    }
+}
--- src/main/java/net/foxes4life/foxclient/mixin/InGameHUDMixin.java
+++ src/main/java/net/foxes4life/foxclient/mixin/InGameHUDMixin.java
@@ -3,6 +3,7 @@ package net.foxes4life.foxclient.mixin;
 import net.foxes4life.foxclient.Main;
 import net.foxes4life.foxclient.configuration.FoxClientSetting;
 import net.foxes4life.foxclient.gui.ArmorHud;
+import net.foxes4life.foxclient.gui.BlockHud;
 import net.foxes4life.foxclient.gui.FoxClientHUD;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
@@ -23,6 +24,7 @@ public class InGameHUDMixin {
 
     private static FoxClientHUD foxClientHUD = null;
     private static ArmorHud armorHud = null;
+    private static BlockHud blockHud = null;
 
     @Inject(at = @At("HEAD"), method = "render")
     public void render(DrawContext context, float tickDelta, CallbackInfo ci) {
@@ -37,5 +39,8 @@ public class InGameHUDMixin {
             if (armorHud == null) armorHud = new ArmorHud(client);
             armorHud.render(context);
         }
+
+        if (blockHud == null) blockHud = new BlockHud(client);
+        blockHud.render(context);
     }
 }
