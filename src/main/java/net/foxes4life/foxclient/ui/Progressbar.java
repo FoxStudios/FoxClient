8ed5e9244df322ad4cf0a134ee2eacb650080bf8
Flux <flustixshinu@gmail.com>
2023-08-17 17:07:09 +0200

    Replace Splash progressbar with new progressbar design

--- src/main/java/net/foxes4life/foxclient/ui/Progressbar.java
+++ src/main/java/net/foxes4life/foxclient/ui/Progressbar.java
@@ -2,11 +2,14 @@ package net.foxes4life.foxclient.ui;
 
 import net.minecraft.client.gui.DrawContext;
 
+import java.awt.*;
+
 public class Progressbar {
     public int x;
     public int y;
     public int width;
     public int height;
+    public float alpha = 1f;
     
     public Progressbar(int x, int y, int width, int height) {
         this.x = x;
@@ -19,14 +22,17 @@ public class Progressbar {
         int progressWidth = (int)Math.ceil(width * progress);
         int cornerSize = (int)(height * .2f);
 
-        context.fill(x + cornerSize, y, x + width - cornerSize, y + cornerSize, 0x33ffffff);
-        context.fill(x, y + cornerSize, x + width, y + height - cornerSize, 0x33ffffff);
-        context.fill(x + cornerSize, y + height - cornerSize, x + width - cornerSize, y + height, 0x33ffffff);
+        int backgroundColor = new Color(1, 1, 1, .2f * alpha).getRGB();
+        int foregroundColor = new Color(1, 1, 1, alpha).getRGB();
 
-        context.fill(x, y + cornerSize, x + progressWidth, y + height - cornerSize, 0xffffffff);
+        context.fill(x + cornerSize, y, x + width - cornerSize, y + cornerSize, backgroundColor);
+        context.fill(x, y + cornerSize, x + width, y + height - cornerSize, backgroundColor);
+        context.fill(x + cornerSize, y + height - cornerSize, x + width - cornerSize, y + height, backgroundColor);
+
+        context.fill(x, y + cornerSize, x + progressWidth, y + height - cornerSize, foregroundColor);
         if (progressWidth > cornerSize) {
-            context.fill(x + cornerSize, y, x + progressWidth - cornerSize, y + cornerSize, 0xffffffff);
-            context.fill(x + cornerSize, y + height - cornerSize, x + progressWidth - cornerSize, y + height, 0xffffffff);
+            context.fill(x + cornerSize, y, x + progressWidth - cornerSize, y + cornerSize, foregroundColor);
+            context.fill(x + cornerSize, y + height - cornerSize, x + progressWidth - cornerSize, y + height, foregroundColor);
         }
     }
 }
