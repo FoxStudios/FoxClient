1b428b169bd3ec4d1621392152d0006e28e66fd5
rooot <hey@rooot.gay>
2023-10-26 00:30:59 +0200

    fix: crash when opening world to LAN
    Signed-off-by: rooot <hey@rooot.gay>

--- src/main/java/net/foxes4life/foxclient/rpc/DiscordMinecraftClient.java
+++ src/main/java/net/foxes4life/foxclient/rpc/DiscordMinecraftClient.java
@@ -2,15 +2,19 @@ package net.foxes4life.foxclient.rpc;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ServerInfo;
 
 public class DiscordMinecraftClient {
     public static State getState(ClientPlayNetworkHandler clientPlayNetworkHandler) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        ServerInfo serverEntry = client.getCurrentServerEntry();
+
         if (clientPlayNetworkHandler != null && clientPlayNetworkHandler.getConnection().isOpen()) {
-            if (MinecraftClient.getInstance().getServer() != null && !MinecraftClient.getInstance().getServer().isRemote()) {
+            if (client.getServer() != null && !client.getServer().isRemote()) {
                 return State.SINGLEPLAYER;
-            } else if (MinecraftClient.getInstance().getCurrentServerEntry().isRealm()) {
+            } else if (serverEntry != null && serverEntry.isRealm()) {
                 return State.REALMS;
-            } else if (MinecraftClient.getInstance().getServer() == null && (MinecraftClient.getInstance().getCurrentServerEntry() == null || !MinecraftClient.getInstance().getCurrentServerEntry().isLocal())) {
+            } else if (client.getServer() == null && (client.getCurrentServerEntry() == null || !client.getCurrentServerEntry().isLocal())) {
                 return State.MULTIPLAYER;
             } else {
                 return State.LAN;
