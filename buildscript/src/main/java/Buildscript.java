8fa31504db39a99aef2b8870f4f015c393ec9ead
rooot <hey@rooot.gay>
2025-01-05 09:02:07 +0100

    feat: deranged xml "parser" for automatic fabric api module getting idk
    Signed-off-by: rooot <hey@rooot.gay>

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -14,9 +14,15 @@ import net.fabricmc.mappingio.tree.MappingTree;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Constants;
+
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Buildscript extends SimpleFabricProject {
 
@@ -100,16 +106,60 @@ public class Buildscript extends SimpleFabricProject {
 
 	@Override
 	public void getModDependencies(ModDependencyCollector d) {
-		String[][] fapiModules = new String[][] {
-				{"fabric-api-base", "0.4.33+fce67b32a0"},
-				{"fabric-key-binding-api-v1", "1.0.38+f4b7e424a0"},
-				{"fabric-lifecycle-events-v1", "2.2.27+b1792f7da0"},
-				{"fabric-resource-loader-v0", "0.11.12+b1792f7da0"},
-				{"fabric-screen-api-v1", "2.0.12+b1792f7da0"},
+		String[] fapiModules = new String[] {
+				"fabric-api-base", "fabric-key-binding-api-v1", "fabric-lifecycle-events-v1", "fabric-resource-loader-v0", "fabric-screen-api-v1"
 		};
 
-		for (String[] module : fapiModules) {
-			d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", module[0], module[1]), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
+		HashMap<String, String> foundFapiModules = new HashMap<>();
+		try {
+			HttpClient client = HttpClient.newHttpClient();
+			HttpRequest request = HttpRequest.newBuilder()
+					.uri(URI.create("https://maven.fabricmc.net/net/fabricmc/fabric-api/fabric-api/" + FABRIC_API_VERSION + "/fabric-api-" + FABRIC_API_VERSION + ".pom"))
+					.header("User-Agent", "brachyuri/0.0.0 (FoxClient Build Script)")
+					.build();
+
+			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+			// what the fuck is an XML parser RAHHHHH
+			String body = response.body();
+			body = body.replace("<", "$$$").replace(">", "$$$").replace("/", "")
+					.replace("$$$dependency$$$", "").replace("\r", "").replace("\n", "")
+					.replace(" ", "")
+					.replace("\t", "");
+			body = body.substring(body.indexOf("$$$dependencies$$$") + 18);
+
+			String[] items = body.split("\\$\\$\\$\\$\\$\\$");
+			String artifactId = "";
+			String version = "";
+
+			for (String item : items) {
+				String type = item.substring(item.lastIndexOf("$$$") + 3);
+				System.out.println(type + " " + item);
+				item = item.replace("$$$"+type, "").replace(type+"$$$", "");
+
+				if (type.equals("artifactId")) {
+					artifactId = item;
+				}
+				if (type.equals("version")) {
+					version = item;
+				}
+				// scope is always the last entries per dependency. hopefully.
+				if (type.equals("scope")) {
+					foundFapiModules.put(artifactId, version);
+				}
+			}
+		} catch (InterruptedException | IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		for (String module : fapiModules) {
+			if (!foundFapiModules.containsKey(module)) {
+				throw new RuntimeException("could not find fapi module" + module);
+			}
+			String moduleVer = foundFapiModules.get(module);
+			System.out.println("--> " + module + ": " + moduleVer);
+
+			d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api", module, moduleVer), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
 		}
 
 		d.addMaven(FabricMaven.URL, new MavenId(FabricMaven.GROUP_ID + ".fabric-api:fabric-api:" + FABRIC_API_VERSION), ModDependencyFlag.RUNTIME, ModDependencyFlag.COMPILE);
