5da13102313e3cdbcf33174ff8067779f96bcfe3
Blexyel <passihd@phoenixts.eu>
2025-01-31 08:28:50 +0100

    fix(translation): use correct toggle hud translation key, add more info for dc rpc
    Signed-off-by: Blexyel <passihd@phoenixts.eu>

--- src/main/java/net/foxes4life/foxclient/MainClient.java
+++ src/main/java/net/foxes4life/foxclient/MainClient.java
@@ -17,7 +17,7 @@ import net.minecraft.client.option.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 
 public class MainClient implements ClientModInitializer {
-    private static final KeyBinding toggleInfoHud = new KeyBinding("key.foxclient.toggle-info-hud", GLFW.GLFW_KEY_F6, "category.foxclient.main");
+    private static final KeyBinding toggleInfoHud = new KeyBinding("key.foxclient.toggle_hud", GLFW.GLFW_KEY_F6, "category.foxclient.main");
     private static final KeyBinding clientConfig = new KeyBinding("key.foxclient.configKey", GLFW.GLFW_KEY_RIGHT_CONTROL, "category.foxclient.main");
 
     public static long deltaTime = 0;
